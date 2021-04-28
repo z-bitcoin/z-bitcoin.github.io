99f471d23a4414a15f75248d18a156d26aea2270
z-bitcoin <82584020+z-bitcoin@users.noreply.github.com>
2021-04-29 02:08:10 +0700

    .

--- countdown/script.js
+++ countdown/script.js
@@ -22,7 +22,7 @@
               countdown = document.getElementById("countdown"),
               content = document.getElementById("content");
 
-          headline.innerText = "COUNTDOWN ZBTC ADD LIQUIDITY PANCAKESWAP V2";
+          headline.innerText = "COUNTDOWN Z-BITCOIN (ZBTC) ADD LIQUIDITY PANCAKESWAP V2";
           countdown.style.display = "none";
           content.style.display = "block";
 
