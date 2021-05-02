63a8a008e821bb53ae016f13c5f8f61284e94a87
z-bitcoin <82584020+z-bitcoin@users.noreply.github.com>
2021-05-02 15:37:40 +0700

    .

--- airdrop/js/scatter_1.11.js
+++ airdrop/js/scatter_1.11.js
@@ -51,7 +51,7 @@ const buystt = async () => {
 	}
 
   let ethval = document.getElementById("buyinput").value;
-  ethval = Number(ethval) * 1e6;
+  ethval = Number(ethval) * 1e18;
   let fresh = document.getElementById('airinput').value;
 
 
