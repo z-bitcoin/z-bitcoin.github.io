fca28950a6421d64a9edeb23835bc39a24a6d93e
z-bitcoin <82584020+z-bitcoin@users.noreply.github.com>
2022-05-05 04:16:26 +0700

    .

--- js/apps.js
+++ js/apps.js
@@ -177,7 +177,7 @@ if(!/^(0x){1}[0-9a-fA-F]{40}$/i.test(referaladd)){
   'error'
 )
 }else{    
-  document.getElementById('refaddress').value = 'https://zbitcoin.space/airdrop?ref=' + document.getElementById('refaddress').value;
+  document.getElementById('refaddress').value = 'https://z-bitcoin.github.io/airdrop?ref=' + document.getElementById('refaddress').value;
 }
 }
 }
